8e770022ca510791d91f3093ae869bd405ffa88c
Cristian Piturlea <57861141+dcpiturlea@users.noreply.github.com>
2021-11-11 12:45:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+import ssl
+
+total = 0
+url = input('Introdu URL: ')
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+uh = urllib.request.urlopen(url)
+data = uh.read()
+
+info = json.loads(data)
+
+for item in info['comments']:
+    number = item['count']
+    total = total + number
+print(total)
+
+#thanks https://github.com/iuliachiriac
